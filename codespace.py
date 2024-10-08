c561bd3155ddca33362f63a23f0a8e334e1ba3f3
Rodrigo RJ <151276298+Rodrigocolorado@users.noreply.github.com>
2024-10-07 23:39:44 -0300

    codespace.py

---
+++ codespace.py
@@ -0,0 +1,5 @@
+busca1 = input("Insira a primeira autorização: ")
+busca2 = input("Insira a segunda informação: ")
+
+busca_concatenada = busca1 + " " + busca2
+print("As informações concatenadas são: ", busca_concatenada)
